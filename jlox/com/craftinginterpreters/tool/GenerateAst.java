068d38c2e8183168f97f8ae268ff6195110efd62
unknown <adammitchell.connect@gmail.com>
2026-09-24 22:50:14 -0700

    Update GenerateAst.java

--- jlox/com/craftinginterpreters/tool/GenerateAst.java
+++ jlox/com/craftinginterpreters/tool/GenerateAst.java
@@ -17,6 +17,7 @@ defineAst(outputDir, "Expr", Arrays.asList(
       "Binary   : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal  : Object value",
+      "Logical  : Expr left, Token operator, Expr right",
       "Unary    : Token operator, Expr right",
       "Variable : Token name"
     ));
@@ -24,8 +25,11 @@ defineAst(outputDir, "Expr", Arrays.asList(
     defineAst(outputDir, "Stmt", Arrays.asList(
       "Block      : List<Stmt> statements",
       "Expression : Expr expression",
+      "If         : Expr condition, Stmt thenBranch," +
+                  " Stmt elseBranch",
       "Print      : Expr expression",
-      "Var        : Token name, Expr initializer"
+      "Var        : Token name, Expr initializer",
+      "While      : Expr condition, Stmt body"
     ));
   }
 
